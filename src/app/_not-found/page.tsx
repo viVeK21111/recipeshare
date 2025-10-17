fe243c5518b0456a32efac189e298a79d22f5994
Vivek <vivekvemula21@gmail.com>
2025-10-17 17:39:41 +0530

    added not found page

--- src/app/_not-found/page.tsx
+++ src/app/_not-found/page.tsx
@@ -1,4 +1,6 @@
 // src/app/not-found.tsx
+'use client'
+
 export default function NotFound() {
     return (
       <div className="min-h-screen flex items-center justify-center bg-white text-gray-800">
